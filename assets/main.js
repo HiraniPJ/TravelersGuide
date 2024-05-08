c369e6b0e091acdbe48af94509090c3b4720e9ed
HiraniPJ <pooja.hirani93@gmail.com>
2024-05-08 13:23:00 +0100

    adjusted the js file and style related to carousel and slideshow

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,23 @@
-$(document).ready(function()
-{
-$('.image-carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 2000
-});
-$('.team-slideshow').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    autoplay: true
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const switchSlide = (container, interval) => {
+        let slides = container.querySelectorAll('img');
+        let currentSlide = 0;
+        setInterval(() => {
+            slides[currentSlide].style.opacity = 0;
+            currentSlide = (currentSlide + 1) % slides.length;
+            slides[currentSlide].style.opacity = 1;
+        }, interval);
+    };
+
+    const imageCarousel = document.querySelector('.image-carousel');
+    const teamSlideshow = document.querySelector('.team-slideshow');
+
+    if (imageCarousel) {
+        switchSlide(imageCarousel, 2000);
+    }
+
+    if (teamSlideshow) {
+        switchSlide(teamSlideshow, 3000);
+    }
 
 });
