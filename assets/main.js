3821265469e16a6c14f6784b5f157439be7e0968
HiraniPJ <pooja.hirani93@gmail.com>
2024-10-20 22:12:41 +0100

    adjusting the nav bar

--- assets/main.js
+++ assets/main.js
@@ -4,8 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('navLinks');
 
+    if (hamburger && navLinks) {
         hamburger.addEventListener('click', function() {
-            navLinks.classList.toggle('active');
+            if (navLinks.style.display === 'block') {
+                navLinks.style.display = 'none';
+            } else {
+                navLinks.style.display = 'block';
+            }
         });
 
 
@@ -67,5 +72,5 @@ document.addEventListener('DOMContentLoaded', function() {
             currentSlide(index + 1);
         });
     });
-
+    }
 });
